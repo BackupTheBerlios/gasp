5d0d036173ba344eb1da9f2e130a79148bcf37f1
romale <romale>
2005-07-27 12:32:51 +0000

    added testDefaultLifecycle, testLookupWithExceptionDataSourceFactory, testLookupWithNullDataSourceFactory, testRegisterNullDataSourceDescriptor, testSetNullDataSourceFactory

--- gasp/plugins/datasource/src/test/java/org/eu/gasp/datasource/DataSourceServiceTest.java
+++ gasp/plugins/datasource/src/test/java/org/eu/gasp/datasource/DataSourceServiceTest.java
@@ -23,6 +23,8 @@
 package org.eu.gasp.datasource;
 
 
+import javax.sql.DataSource;
+
 import junit.framework.TestCase;
 
 
@@ -46,15 +48,92 @@ public class DataSourceServiceTest extends TestCase {
             // expected
         }
     }
-    
-    
+
+
     public void testLookupNullId() {
         final DataSourceService dss = new DataSourceService();
         try {
             dss.lookup(null);
             fail("Expected exception was not thrown");
-        } catch(Exception e) {
+        } catch (Exception e) {
             // expected
         }
     }
+
+
+    public void testDefaultLifecycle() {
+        final DataSourceService dss = new DataSourceService();
+        dss.setDataSourceFactory(new MockDataSourceFactory());
+
+        final String id = "test";
+        dss.register(new DataSourceDescriptor(id, "jdbc:test://test",
+                "test.Test", "testuser", "testpasswd"));
+        final DataSource ds = dss.lookup(id);
+        assertNotNull(ds);
+
+        dss.unregister(id);
+        try {
+            dss.lookup(id);
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
+
+    public void testLookupWithNullDataSourceFactory() {
+        final DataSourceService dss = new DataSourceService();
+        dss.setDataSourceFactory(new NullDataSourceFactory());
+
+        final String id = "test";
+        dss.register(new DataSourceDescriptor(id, "jdbc:test://test",
+                "test.Test", "testuser", "testpasswd"));
+
+        try {
+            dss.lookup(id);
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
+
+    public void testRegisterNullDataSourceDescriptor() {
+        final DataSourceService dss = new DataSourceService();
+        try {
+            dss.register(null);
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
+
+    public void testSetNullDataSourceFactory() {
+        final DataSourceService dss = new DataSourceService();
+        try {
+            dss.setDataSourceFactory(null);
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
+
+    public void testLookupWithExceptionDataSourceFactory() {
+        final DataSourceService dss = new DataSourceService();
+        dss.setDataSourceFactory(new ExceptionDataSourceFactory());
+
+        final String id = "test";
+        dss.register(new DataSourceDescriptor(id, "jdbc:test://test",
+                "test.Test", "testuser", "testpasswd"));
+
+        try {
+            dss.lookup(id);
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
 }
